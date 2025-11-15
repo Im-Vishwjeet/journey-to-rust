f2cbfe7627d9872627a96947886acd0f91db346d
Im-Vishwjeet <vishwjeetmeena2811@gmail.com>
2025-11-15 23:16:12 +0530

    feat: Add rust chapter 4 Conditional Statements.

---
+++ 4_Conditional_Statements/src/main.rs
@@ -0,0 +1,34 @@
+fn main() {
+    let n = 5;
+
+    if n < 0 {
+        print!("{} is negative", n);
+    } else if n > 0 {
+        print!("{} is positive", n);
+    } else {
+        print!("{} is zero", n);
+    }
+
+    let big_n =
+        if n <= 9 && n >= -9 {
+            println!(", and is a small number, increase ten-fold");
+
+            // This expression returns an `i32`.
+            10 * n
+        } else {
+            println!(", and is a big number, halve the number");
+
+            // This expression must return an `i32` as well.
+            n / 2
+            // TODO ^ Try suppressing this expression with a semicolon.
+        };
+    //   ^ Don't forget to put a semicolon here! All `let` bindings need it.
+
+    println!("{} -> {}", n, big_n);
+
+    /* Note: 
+    1. Rust does not have a dedicated ternary operator.
+    2. Statements are instructions that perform some action and do not return a value. "n / 2;" is a statement.
+    3. Expressions evaluate to a resultant value. "10 * n" is an expression.
+    */
+}
